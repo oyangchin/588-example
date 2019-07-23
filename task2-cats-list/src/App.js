89513b97c687d565466afc6ab9e04a72706c0c72
oyang <oyang0124@gmail.com>
2019-07-23 15:22:57 +0800

    [2] stateless component change to stateful component
    function change to class

--- task2-cats-list/src/App.js
+++ task2-cats-list/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Cats List</h1>
-      <input className="search-box" type="search" placeholder="search cats" />
-      <div className="card-list">
-        <div className="card-container">
-          <img alt="cat" src="https://robohash.org/6?set=set4&size=180x180" />
-          <h2> Mrs. Dennis Schulist </h2>
-          <p> Karley_Dach@jasper.info </p>
-        </div>
-        <div className="card-container">
-          <img alt="cat" src="https://robohash.org/4?set=set4&size=180x180" />
-          <h2> Patricia Lebsack </h2>
-          <p> Julianne.OConner@kory.org </p>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Cats List</h1>
+        <input className="search-box" type="search" placeholder="search cats" />
+        <div className="card-list">
+          <div className="card-container">
+            <img alt="cat" src="https://robohash.org/6?set=set4&size=180x180" />
+            <h2> Mrs. Dennis Schulist </h2>
+            <p> Karley_Dach@jasper.info </p>
+          </div>
+          <div className="card-container">
+            <img alt="cat" src="https://robohash.org/4?set=set4&size=180x180" />
+            <h2> Patricia Lebsack </h2>
+            <p> Julianne.OConner@kory.org </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
