78a2c84887f530fd269ad5ae51b2138dd1db5bd5
oyang <oyang0124@gmail.com>
2019-07-23 15:18:03 +0800

    [2] add cat list and card

--- task2-cats-list/src/App.js
+++ task2-cats-list/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,6 +6,18 @@ function App() {
     <div className="App">
       <h1>Cats List</h1>
       <input className="search-box" type="search" placeholder="search cats" />
+      <div className="card-list">
+        <div className="card-container">
+          <img alt="cat" src="https://robohash.org/6?set=set4&size=180x180" />
+          <h2> Mrs. Dennis Schulist </h2>
+          <p> Karley_Dach@jasper.info </p>
+        </div>
+        <div className="card-container">
+          <img alt="cat" src="https://robohash.org/4?set=set4&size=180x180" />
+          <h2> Patricia Lebsack </h2>
+          <p> Julianne.OConner@kory.org </p>
+        </div>
+      </div>
     </div>
   );
 }
